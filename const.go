dd76d062a45228c11fe92076b634292670feb10d
Choestelus <sprintf.null@gmail.com>
2015-06-16 13:42:54 +0700

    Initial project commit

---
+++ const.go
@@ -0,0 +1,15 @@
+package main
+
+const LineThriftServer = "gd2.line.naver.jp"
+const LineOSServer = "os.line.naver.jp"
+const LineOSURL = "https://os.line.naver.jp/"
+const LineStickerURL = "http://dl.stickershop.line.naver.jp/products/"
+const LineVerificationURL = "https://gd2.line.naver.jp/Q"
+
+const LineLoginPath = "/api/v4/TalkService.do"
+const LineCommandPath = "/S4"
+const LinePollPath = "/P4"
+const LineShopPath = "/SHOP4"
+
+const LineUserAgent = "purple-line (LINE for libpurple/Pidgin)"
+const LineApplication = "DESKTOPWIN\t3.2.1.83\tWINDOWS\t5.1.2600-XP-x64"
