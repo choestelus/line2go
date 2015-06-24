e794b0519a5b4266afa62288d31aa21aacda7b75
Choestelus <sprintf.null@gmail.com>
2015-06-24 15:49:27 +0700

    completed send authentication flow [400 bad request]

--- const.go
+++ const.go
@@ -14,4 +14,4 @@ const LineShopPath = "/SHOP4"
 const LineUserAgent = "purple-line (LINE for libpurple/Pidgin)"
 const LineApplication = "DESKTOPWIN\t3.2.1.83\tWINDOWS\t5.1.2600-XP-x64"
 
-const UseHTTP = true
+const UseHTTP = false
