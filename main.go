84e570c75915fefcd66bdaff630f6277e3eeb059
Choestelus <sprintf.null@gmail.com>
2015-06-24 15:49:27 +0700

    completed send authentication flow [400 bad request]

--- main.go
+++ main.go
@@ -1,59 +1,71 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/fatih/color"
+	"github.com/parnurzeal/gorequest"
+	"io/ioutil"
 	"line"
 	"log"
+	"os"
+	"strconv"
 )
-
-var _ = thrift.ZERO
-var _ = fmt.Printf
 
 type ThriftClient struct {
 	*line.TalkServiceClient
 }
 
+var _ = tls.Config{}
+
 func main() {
 	var err error
-	var transport thrift.TTransport
-	var httpTransport thrift.TTransport
+	var transport *thrift.StreamTransport
 	var talkClient *line.TalkServiceClient
-
-	_ = httpTransport
-	// var talkClient ThriftClient
-	urlPath := "http://" + LineThriftServer + ":443" + LineLoginPath
+	var message bytes.Buffer
 
 	ident := "choestelus@gmail.com"
 	pwd := ""
-	cfg := new(tls.Config)
-	log.Printf("openning socket to: %v", urlPath)
 
-	if UseHTTP {
-		log.Printf("using http protocol")
-		transport, err = thrift.NewTHttpClient(urlPath)
-	} else {
-		log.Printf("using secure socket layer")
-		transport, err = thrift.NewTSSLSocket(urlPath, cfg)
-	}
+	dev0, _ := os.Open("/dev/zero")
+
+	transport = thrift.NewStreamTransport(dev0, &message)
 	if err != nil {
-		log.Fatalln("Error opening secure socket : ", err)
+		log.Fatalln("Error opening transport connection: ", err)
 	}
 
 	transportFactory := thrift.NewTTransportFactory()
-	transport = transportFactory.GetTransport(transport)
-	if err := transport.Open(); err != nil {
-		log.Fatalln("could not open connection", err)
-	}
-	defer transport.Close()
+	wrappedTransport := transportFactory.GetTransport(transport)
 
-	talkClient = line.NewTalkServiceClientFactory(transport, thrift.NewTCompactProtocolFactory())
+	talkClient = line.NewTalkServiceClientFactory(wrappedTransport, thrift.NewTCompactProtocolFactory())
 
 	result, err := talkClient.LoginWithIdentityCredentialForCertificate(line.IdentityProvider_LINE, ident, pwd, true, "127.0.0.1", "goLINE", "emp")
 	if err != nil {
-		log.Fatalln("Error logging in: ", err)
+		log.Println("Error logging in: ", err)
 	}
 
-	log.Printf("result: %v", result)
+	log.Printf("body message : %T [%v]\n", message, message.String())
+	fmt.Fprintf(ioutil.Discard, "result: %v", result)
+
+	greenBold := color.New(color.FgGreen).Add(color.Bold).SprintFunc()
+
+	req := gorequest.New() //.TLSClientConfig(&tls.Config{})
+	resp, body, errs := req.Post("http://"+LineThriftServer+LineLoginPath).
+		Set("connection", "keep-alive").
+		Set("Content-Type", "application/x-thrift").
+		Set("User-Agent", "purple-line (LINE for libpurple/pidgin)").
+		Set("X-Line-Application", "DESKTOPWIN\t3.2.1.83\tWINDOWS\t5.1.2600-XP-x64").
+		SendString(message.String()).
+		End()
+	if errs != nil {
+		log.Fatalf("Fatal : %v", errs)
+	}
+
+	log.Printf("status : %v code = [%v]\n", greenBold(resp.Status), greenBold(strconv.Itoa(resp.StatusCode)))
+	log.Printf("resp = [%v]\n", resp.Header)
+	fmt.Printf("%v %v\nlength = [%v]\n", greenBold("respTLS: "), resp.TLS, resp.ContentLength)
+	fmt.Printf("%v%v\n", greenBold("------body------\n"), body)
+
 }
