70f2ac3acf0e56dd570a7fe4a42adf22d1cfd88a
Choestelus <sprintf.null@gmail.com>
2015-07-02 14:37:51 +0700

    use functions instead of hardcoded procedural statements

--- main.go
+++ main.go
@@ -18,61 +18,45 @@ var (
 func main() {
 	fmt.Fprintf(ioutil.Discard, "")
 	var err error
-	var commandTransport thrift.TTransport
 
 	ident := "choestelus@gmail.com"
 	pwd := "suchlinemuchwow@443"
 
-	url2 := "https://" + LineThriftServer + LineCommandPath
-	commandTransport, _ = thrift.NewTHttpPostClient(url2)
-
 	// type assertion
-	commandTrans := commandTransport.(*thrift.THttpClient)
-
-	transportFactory := thrift.NewTTransportFactory()
 
 	result, err := LoginLine(ident, pwd)
 	if err != nil {
 		log.Fatalln("Error logging in: ", err)
 	}
+	// TODO: handle pinverfication request
+	if result.GetTypeA1() == line.LoginResultType_REQUIRE_DEVICE_CONFIRM {
+		log.Fatalf("error: need pin verification; not handle yet")
+		// Code here:
+		// create blank http request
+	}
 
 	// Initialize new client from received authtoken
 	token = result.GetAuthToken()
-	commandTrans.SetHeader("X-Line-Access", token)
-	commandTrans.SetHeader("User-Agent", AppUserAgent)
-	commandTrans.SetHeader("X-Line-Application", LineApplication)
-	commandTrans.SetHeader("Connection", "Keep-Alive")
 
-	wrappedCommandTransport := transportFactory.GetTransport(commandTrans)
-	commandClient := line.NewTalkServiceClientFactory(wrappedCommandTransport, thrift.NewTCompactProtocolFactory())
+	// commandClient := line.NewTalkServiceClientFactory(wrappedCommandTransport, thrift.NewTCompactProtocolFactory())
+	commandClient := GetCommandClient(token)
 
 	lastOpRevision, err := commandClient.GetLastOpRevision()
 	if err != nil {
 		log.Fatalln("Error GetLastOpRevision: ", err)
 	}
 
-	// proof that GetHeader is useless to get header from HTTP response messages.
-	// fmt.Printf("\ntest get-header: [%v]\n", loginClient.Transport.(*thrift.THttpClient).GetHeader("X-Line-Application"))
-	// fmt.Printf("\ntest get-header: [%v]\n", loginClient.Transport.(*thrift.THttpClient).GetHeader("X-Lcr"))
-
-	// Workaround: use this instead
-	// disclamier: น่าจะ Non thread-safe if there are more than 1 go-routine call using loginClient instance
-
 	prettyResult := fmt.Sprint(greenBold(result.String()))
 	log.Printf("Type: [%T], result: %v\n", result, prettyResult)
 
 	printLoginResult(result)
 	fmt.Printf("GetLastOpRevision = %v\n", greenBold(strconv.FormatInt(lastOpRevision, 10)))
+
+	// Get X-LS value from response header
 	Line_X_LS = commandClient.Transport.(*thrift.THttpClient).GetResponse().Header.Get("X-LS")
 	fmt.Printf("\nX-LS: [%v]\n", cyanBold(Line_X_LS))
 
-	// TODO: add X-LS request header, remove other unused headers
-	commandClient.Transport.(*thrift.THttpClient).DelHeader("X-Line-Access")
-	commandClient.Transport.(*thrift.THttpClient).DelHeader("User-Agent")
-	commandClient.Transport.(*thrift.THttpClient).DelHeader("X-Line-Application")
-	commandClient.Transport.(*thrift.THttpClient).DelHeader("Connection")
-
-	commandClient.Transport.(*thrift.THttpClient).SetHeader("X-LS", Line_X_LS)
+	SetHeaderForClientReuse(commandClient, Line_X_LS)
 
 	// TODO: GetProfile
 	profile, err := commandClient.GetProfile()
@@ -93,11 +77,4 @@ func main() {
 	// TODO: GetMessageBoxCompactWrapUpList
 	wrapuplist, err := commandClient.GetMessageBoxCompactWrapUpList(1, 50)
 	fmt.Printf("%v\n", wrapuplist.String())
-
-	// TODO: handle pinverfication request
-	if result.GetTypeA1() == line.LoginResultType_REQUIRE_DEVICE_CONFIRM {
-		log.Fatalf("error: need pin verification; not handle yet")
-		// Code here:
-		// create blank http request
-	}
 }
