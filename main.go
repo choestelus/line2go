19b2c0bcd9981466bfec73d8a40aca886ee382a1
Choestelus <sprintf.null@gmail.com>
2015-07-02 12:05:04 +0700

    use LoginLine function instead of hard coding process

--- main.go
+++ main.go
@@ -18,43 +18,20 @@ var (
 func main() {
 	fmt.Fprintf(ioutil.Discard, "")
 	var err error
-	var loginClient *line.TalkServiceClient
-	var loginTransport thrift.TTransport
 	var commandTransport thrift.TTransport
 
 	ident := "choestelus@gmail.com"
 	pwd := "suchlinemuchwow@443"
 
-	url := "https://" + LineThriftServer + LineLoginPath
 	url2 := "https://" + LineThriftServer + LineCommandPath
-	loginTransport, _ = thrift.NewTHttpPostClient(url)
 	commandTransport, _ = thrift.NewTHttpPostClient(url2)
 
 	// type assertion
-	loginTrans := loginTransport.(*thrift.THttpClient)
 	commandTrans := commandTransport.(*thrift.THttpClient)
 
-	// set specific header
-	loginTrans.SetHeader("User-Agent", AppUserAgent)
-	loginTrans.SetHeader("X-Line-Application", LineApplication)
-	loginTrans.SetHeader("Connection", "Keep-Alive")
-
 	transportFactory := thrift.NewTTransportFactory()
-	wrappedloginTransport := transportFactory.GetTransport(loginTrans)
 
-	loginClient = line.NewTalkServiceClientFactory(
-		wrappedloginTransport,
-		thrift.NewTCompactProtocolFactory())
-
-	result, err := loginClient.LoginWithIdentityCredentialForCertificate(
-		line.IdentityProvider_LINE,
-		ident,
-		pwd,
-		true,
-		"127.0.0.1",
-		AppUserAgent,
-		"")
-
+	result, err := LoginLine(ident, pwd)
 	if err != nil {
 		log.Fatalln("Error logging in: ", err)
 	}
