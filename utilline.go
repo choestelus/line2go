4e567b4548f38e79a9894f8f734a5325b2bfb7c0
Choestelus <sprintf.null@gmail.com>
2015-07-02 12:00:39 +0700

    Separate login function

--- utilline.go
+++ utilline.go
@@ -2,11 +2,56 @@ package main
 
 import (
 	"fmt"
-	"github.com/fatih/color"
 	"line"
+	"log"
+
+	"git.apache.org/thrift.git/lib/go/thrift"
+
+	"github.com/fatih/color"
 )
 
 var cyanBold = color.New(color.FgCyan).Add(color.Bold).SprintFunc()
+var greenBold = color.New(color.FgGreen).Add(color.Bold).SprintFunc()
+
+func LoginLine(ident string, ptPassword string) (*line.LoginResult_, error) {
+	loginURL := HTTPPrefix + LineThriftServer + LineLoginPath
+	loginTransport, err := thrift.NewTHttpPostClient(loginURL)
+	if err != nil {
+		log.Fatalln("Error Creating HTTP Client: ", err)
+	}
+	loginTrans := loginTransport.(*thrift.THttpClient)
+
+	// set specific header
+	loginTrans.SetHeader("User-Agent", AppUserAgent)
+	loginTrans.SetHeader("X-Line-Application", LineApplication)
+	loginTrans.SetHeader("Connection", "Keep-Alive")
+
+	wrappedLoginTrans := thrift.NewTTransportFactory().GetTransport(loginTrans)
+
+	loginClient := line.NewTalkServiceClientFactory(wrappedLoginTrans, thrift.NewTCompactProtocolFactory())
+
+	// Parameters:
+	//  - IdentityProvider
+	//  - Identifier
+	//  - Password
+	//  - KeepLoggedIn
+	//  - AccessLocation
+	//  - SystemName
+	//  - Certificate
+	result, err := loginClient.LoginWithIdentityCredentialForCertificate(
+		line.IdentityProvider_LINE,
+		ident,
+		ptPassword,
+		true,
+		"127.0.0.1",
+		AppUserAgent,
+		"")
+	if err != nil {
+		log.Fatalln("Error logging in: ", err)
+	}
+
+	return result, err
+}
 
 func printLoginResult(result *line.LoginResult_) {
 	fmt.Println("--------------------------------------------------------------------------------")
